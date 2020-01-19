7acca3b70c6855d18a89e5ce04aeddd4162af66e
lamia <chafaa_lamia@hotmail.com>
2020-01-19 10:13:24 +0100

    add header to index

--- index.php
+++ index.php
@@ -2,5 +2,6 @@
 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium rerum quam eveniet numquam accusamus, 
 rem quae voluptates doloremque voluptas. Magni nulla harum necessitatibus itaque iusto eaque earum placeat excepturi saepe?</p>
 
-<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium rerum quam eveniet numquam accusamus, 
-rem quae voluptates doloremque voluptas. Magni nulla harum necessitatibus itaque iusto eaque earum placeat excepturi saepe?</p>
+<header>
+    header
+</header>
